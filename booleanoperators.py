1a4b18f7f41cfbc152ad4c9ce0ce252728330400
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-08 08:25:25 +0100

    boolean operators

---
+++ booleanoperators.py
@@ -0,0 +1,6 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 5 in (6,7,8,5)
+True
+>>> 5 in (6,7,8)
+False
