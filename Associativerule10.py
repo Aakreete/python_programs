ed298b9079d443c0a919e4e7fc250749367840e9
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-08 08:36:44 +0100

    adding boolean

---
+++ Associativerule10.py
@@ -0,0 +1,45 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#associative rule is the rule which helps to find the answer according to the rule.
+8*(4-2)
+16
+>>> 9*(0-0)
+0
+>>> 0*(89*189999)
+0
+>>> 3*(100939485579*840184018501)
+254423227857563876091237
+>>> 9**5
+59049
+>>> 9**5*5
+295245
+>>> 25**9
+3814697265625
+>>> 9**5
+59049
+>>> 59049*5
+295245
+>>> 
+>>> 7**3**9
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    7**3**9
+ValueError: Exceeds the limit (4300) for integer string conversion; use sys.set_int_max_str_digits() to increase the limit
+>>> 3**2**4
+43046721
+>>> 3**2
+9
+>>> 9**4
+6561
+>>> 2**4
+16
+>>> 16**3
+4096
+>>> 4**2
+16
+>>> 16*3
+48
+>>> 16**3
+4096
+>>> 4096*16
+65536
--- booleanoperators.py
+++ booleanoperators.py
@@ -1,6 +1,32 @@
 Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> 5 in (6,7,8,5)
+>>> #Membership in python
+>>> #in and not in
+>>> 5 in {6,7,8,9}
+False
+>>> 19.5 in {2,19.55, 4}
+False
+>>> 18.6 in {7, 18.61}
+False
+>>> 3 in {3,6}
 True
->>> 5 in (6,7,8)
+>>> 7 not in {8,9,0}
+True
+>>> 8 not in {10,11,8}
+False
+>>> 
+>>> #Boolean operators
+>>> #and , or , not. Example below:
+>>> #boolean operators are used to combine two or more statements,
+>>> 4>5 and 2==2
+False
+>>> 4<5 and 6==6
+True
+>>> 7>2 and 9==5
+False
+>>> 8>5 or 9==5
+True
+>>> 8<6 or 9==4
+False
+>>> not [15<20]
 False
