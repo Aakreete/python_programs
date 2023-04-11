cd3cee7468762509913bb5c05de841cd40394fc1
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-11 21:05:15 +0100

    arithmetic series of multiples of 9

---
+++ arithmeticseries.py
@@ -0,0 +1,5 @@
+#to print arithmetic series like 9, 18, 27, 36, 45, 54, 63, 72, 81, 90
+count = 9
+while count <=90:
+    print(count)
+    count=count+9
---
+++ squarenumber.py
@@ -0,0 +1,5 @@
+count=1
+while count <=5:
+ print(count*count)
+ count=count+1
+#don't understand how this answer came but it did.
