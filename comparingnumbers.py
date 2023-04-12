80c6d2dfd8c77f699959f3bc0130a7b3a713809d
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-12 21:57:50 +0100

    comparing numbers

---
+++ comparingnumbers.py
@@ -0,0 +1,10 @@
+#to find the numebers which one is greater
+a=9
+b=10
+c=20
+if a>b and a>c:
+    print("a is greater")
+if b>c and b>a:
+    print("b is greater")
+else:
+    print("c is greatest among three of them")
