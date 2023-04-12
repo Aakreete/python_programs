4bfc2857a4f8fe4e6a1efa51828e2160c59c1aac
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-12 17:05:21 +0100

    series 3

--- series3.py
+++ series3.py
@@ -1,6 +1,17 @@
-#this program is to print -1 , -4, -7, -10, -13, -16, -19
-count= -1
-while count <= -19:
-    print(count)
-    count= count-3
-    #got to find solution for this one
+# this program is to print -1 , 4, -7, 10, -13, 16, -19
+count1 = 1
+count2 = 0
+a = 5
+initialvalue = -1
+helpervalue = 0
+print(initialvalue)
+while count1 <= 7:
+    helpervalue = (a*count1+count2)
+    if (count1 % 2 == 0):
+        initialvalue=(initialvalue-helpervalue)
+        print(initialvalue)
+    else:
+        initialvalue= (initialvalue+helpervalue)
+        print(initialvalue)
+    count1 = count1+1
+    count2 = count2+1
