d80fc5e8702a726d3d45ffeda9879068e4c33020
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-13 21:38:27 +0100

    dividing two given numbers

---
+++ dividing.py
@@ -0,0 +1,5 @@
+#basically,here we are going to divide the two given numbers
+valuea=int(input("enter any number that youu prefer "))
+valueb=int(input("enter any number that you prefer "))
+divide=valuea/valueb
+print(divide)
