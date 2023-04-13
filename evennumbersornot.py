c40883179263aedb7866d1213d62eef445ea4d2d
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-13 21:30:20 +0100

    even numbers or not

---
+++ evennumbersornot.py
@@ -0,0 +1,6 @@
+#this program is to find out whether it's a even numeber or not.
+num=int(input("enter any numbers that you prefer or like  "))
+if (num % 2)==0:
+    print("Woooo!!!!!!It's a even number")
+elif (num%2)!=0:
+    print("Oppppppssss!!!!It's not a even number")
