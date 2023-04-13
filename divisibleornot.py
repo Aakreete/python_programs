825f6d99c574ed7c7fcfd1615791371a70c2d754
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-13 22:01:10 +0100

    divisible or not by 5

---
+++ divisibleornot.py
@@ -0,0 +1,7 @@
+#this program is to find out whether the input is divided by 5 and 3 or not
+value=int(input("enter any value you like "))
+if value%5==0:
+    print("The number is divisible by 5.")
+elif value%5!=0:
+    print("the number is not divisible by 5.")
+
