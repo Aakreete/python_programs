6b8d6fbbd56ce2866a0d41d9e2ee3f67a9e8955d
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-13 22:06:09 +0100

    odd numbers or not

---
+++ oddnumbers.py
@@ -0,0 +1,7 @@
+#this program is to find out whether it's a odd number or not
+numb=int(input("enter any number that you want "))
+if numb%2!=0:
+    print("It's a odd number")
+if numb%2==0:
+    print("It's not a odd number")
+
