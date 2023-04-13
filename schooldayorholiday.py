4a9727182f8f2272856960b2e80c18d83866f543
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-13 21:13:43 +0100

    finding out whether schoolday or holiday

---
+++ schooldayorholiday.py
@@ -0,0 +1,17 @@
+#this program is to find out whether it is holiday for children 98
+#or it's a school day for their children
+days=str(input("enter any days of the week"))
+if days=="Monday":
+    print("Schoolday")
+elif days=="Tuesday":
+    print("Schoolday")
+elif days=="Wednesday":
+    print("Holiday")
+elif days=="Thursday":
+    print("Schoolday")
+elif days=="Friday":
+    print("Schoolday")
+elif days=="Saturday":
+    print("Holiday")
+else:
+    print("please, enter a valid day")
