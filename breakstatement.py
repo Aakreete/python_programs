73705b0501efa5a92ccd60870b8bb97482c6eacc
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-15 21:22:47 +0100

    break statement

---
+++ breakstatement.py
@@ -0,0 +1,9 @@
+#this program is to see how the break statement works out.
+count=0
+while count<=6:
+    if count==4:
+        break
+    else:
+        print(count)
+        count=count+1
+print("thanks")
