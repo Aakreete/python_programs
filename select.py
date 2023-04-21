a45e40c1e6e6fdbefa5ff687c2991ef6a3ac1de7
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-21 20:56:47 +0100

    select operations in python

---
+++ select.py
@@ -0,0 +1,19 @@
+name="bhusal"
+list1=[10, 20,30,26,78.6]
+tuple1=(10, 10.5, 6.7,9.6)
+
+print(len(name))
+
+print(len(list1))
+
+print(len(tuple1))
+
+name[0]
+print(name[0])
+
+list1[3]
+print(list1[3])
+
+tuple1[2]
+print(tuple1[2])
+
