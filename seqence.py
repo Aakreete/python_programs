3ace438c057ceb79d007519db26cd42fa8f161be
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-21 20:52:33 +0100

    length operation in python

---
+++ seqence.py
@@ -0,0 +1,9 @@
+#there are 10sewuences in python
+#length, slice, select, count, index, membership, concatenation, min value, 
+#max value, sum
+name="aakreetiie"
+list1=[10, 20,30,26,78.6]
+tuple1=(10, 10.5, 6.7,9.6)
+print(len(name))
+print(len(list1))
+print(len(tuple1))
