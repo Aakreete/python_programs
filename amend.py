104ac43e78423e61361fcd1a6f9be8a1cd76de2a
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-21 20:00:09 +0100

    amend operation in python

---
+++ amend.py
@@ -0,0 +1,6 @@
+animalsname=['cat', 'dog', 'binoddonkey', 'monkey']
+print(animalsname)
+animalsname.append('lionnnn')
+print(animalsname)
+animalsname.append('tiger the dangerous animal among all of them')
+print(animalsname)
