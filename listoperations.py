0667f6f90f764acaa0211af45b82d96b1c9251af
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-21 20:18:07 +0100

    list operations in python

---
+++ listoperations.py
@@ -0,0 +1,20 @@
+fruits=['Apple', 'Banana', 'Oranges']
+print(fruits)
+
+fruits.insert(1,'Avocado')  #insert
+print(fruits)
+
+fruits.sort()          #sort
+print(fruits)
+
+fruits.append("Papaya")   #append
+print(fruits)
+
+fruits.reverse()           #reverse
+print(fruits)
+
+del fruits[1]           #delete
+print(fruits)
+
+fruits[2]="grapes"          #replace
+print(fruits)
