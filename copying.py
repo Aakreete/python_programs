266fcf079b7c4fe13abd803ebfdd22f7a2a9e53a
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-22 21:12:42 +0100

    copying operation in python

---
+++ copying.py
@@ -0,0 +1,10 @@
+snapchat=['streaks', 10, 20, 50, 68, 46, 484]
+list1=snapchat
+print(list1)
+list1[3]='you must try streaks on snapchat its so fun and amazing'
+print(list1)
+list2= list (list1)
+print(list2)
+list1[1]=0
+print(list1)
+print(list2)
