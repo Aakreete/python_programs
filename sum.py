9555353e474a31f9b89f86105849872d6b3d2898
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-22 11:17:59 +0100

    sum operation in python

---
+++ sum.py
@@ -0,0 +1,8 @@
+numbers=[20, 15, 6.5, 6.8, 8.9746]
+print(numbers)
+c=['hello to the word', 'how u doing all of you']
+print(numbers+c)
+tuple=[99.67783883, 4737, 38928, 373, 6, 8]
+print(numbers+c+tuple)
+print(sum(numbers))
+print(sum(tuple))
