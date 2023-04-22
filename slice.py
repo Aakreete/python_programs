6cbfe371787a9f05ab5d8601c1aa152dae766772
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-22 10:03:54 +0100

    slice operations in python

---
+++ slice.py
@@ -0,0 +1,10 @@
+name='sedrina'
+print(name)
+listofgames=['volleyball', 'football', 'cricket', 'basketball']
+print(listofgames)
+print(listofgames[:2])
+print(name[1:])
+
+listofanimals=['donkey', 'monkey', 'rhino', 'elephant', 'tiger']
+print(listofanimals)
+print(listofanimals[3:])
