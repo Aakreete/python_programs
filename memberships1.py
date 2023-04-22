0307b3ebbeff8c46d7e56709010e68e5b4a080b8
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-22 10:58:44 +0100

    memberships in python

---
+++ memberships1.py
@@ -0,0 +1,10 @@
+#memberships operation are in and not in.
+#for example:
+name="priyanshikaa"
+print(name)
+print("p" in name)
+
+placename='birmingham'
+print(placename)
+print('i' in placename)
+print('s' in placename)
