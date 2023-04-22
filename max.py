44ce3915aa01478f69c4fef12566cf78f191cf14
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-22 11:15:59 +0100

    maximum operation in python

---
+++ max.py
@@ -0,0 +1,9 @@
+numbers=[20, 15, 6.5, 6.8, 8.9746]
+print(numbers)
+c=['hello to the word', 'how u doing all of you']
+print(numbers+c)
+tuple=[99.67783883, 84764.3383, 283, 38383,74]
+print(numbers+c+tuple)
+print(max(numbers))
+print(max(c))
+print(max(tuple))
