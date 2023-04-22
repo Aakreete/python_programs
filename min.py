739294ac8d840f094b70cc8ff592fa9328e8c3f0
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-22 11:14:23 +0100

    minimun operation in python

---
+++ min.py
@@ -0,0 +1,9 @@
+numbers=[20, 15, 6.5, 6.8, 8.9746]
+print(numbers)
+c=['hello to the word', 'how u doing all of you']
+print(numbers+c)
+tuple=[99.67783883]
+print(numbers+c+tuple)
+print (min(numbers))
+print(min(c))
+
