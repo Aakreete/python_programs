24bb55d89512b9aa9ac7bad277010252896462e2
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-22 21:30:29 +0100

    average functions in python

---
+++ functions.py
@@ -0,0 +1,13 @@
+#example for the function
+#function defination
+def avg(n1,n2,n3,n4):
+    return (n1+n2+n3+n4/4.0)
+print('welcome to this amazing world')
+result1=avg(10,20,50,80)
+result2=avg(2.33,6.77,54.33,77)
+result3=avg(9.2,0.4,7.45,34)
+result4=avg(1.34,5.56,24.3,3.2)
+print('The average number for result1 is', result1)
+print('The average number for result2 is', result2)
+print('The average number for result3 is', result3)
+print('The average number for result1 is', result4)
