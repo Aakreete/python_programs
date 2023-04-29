77a5e33919ffc8b93ffa5b87e184d5f35f7133af
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-29 21:32:14 +0100

    exception handling in python

---
+++ exceptiom.py
@@ -0,0 +1,8 @@
+import math
+num=int(input('please enter the factorial of :'))
+try:
+    result=math.factorial(num)
+    print(result)
+except:
+    print("can't compute the factorial of negative numbers cause of some reason")
+    
