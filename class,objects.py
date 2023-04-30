956ea8beaa18004330b5641492076abd5bb11097
Aakriti Bhusal <aakriti2061@gmail.com>
2023-04-30 11:36:00 +0100

    Class and objects in python

---
+++ class,objects.py
@@ -0,0 +1,16 @@
+#class and objects in python
+#example class is the one which comes before making the object
+#like house is the object and the map for making the house is class
+#function
+def display(name):
+    print("hello", name)
+    return
+#main
+display("Aakriti")
+display("Anapurni")
+
+class person:
+    pass
+#main
+p=person()
+print(p)
