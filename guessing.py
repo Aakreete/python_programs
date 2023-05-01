15d1529966389cd52cce6b6b4d1fb53b77311d98
Aakriti Bhusal <aakriti2061@gmail.com>
2023-05-01 11:47:30 +0100

    Guessing the game

---
+++ guessing.py
@@ -0,0 +1,9 @@
+#this is a guessing game
+print("Welcome guys")
+aakriti=input("Guess the number: ")
+guess=int(aakriti)
+if guess==5:
+    print(" Congratulationss !!you won!")
+else:
+    print("you loose")
+    print("game over")
